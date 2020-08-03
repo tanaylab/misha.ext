cf47d56d9b7f13eb71dede53ce9aceb0ae9704f3
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2020-08-03 16:26:24 +0300

    added functions to transform intervals to matrix. Added arguments to methylation extraction

--- R/methylation.R
+++ R/methylation.R
@@ -7,13 +7,16 @@
 #' @param d_expand smooth the methylation signal \code{d_expand} bp from each side (optional).
 #' @param join_intervals add the intervals to the returned data frame (using left_join)
 #' @param extract_meth_calls extract also methylation calls (".meth" tracks)
+#' @param avg_tracks tracks of average methylation (for tracks that do not have ".cov" and ".meth" tracks)
+#' @param avg_tracks_names names of tracks in \code{avg_tracks} (optional)
+#' @param min_cov minimal coverage per track. If the coverage is below this number the methylation values would be changed to NA.
 #' @param ... additional arguments to \link[misha]{gextract}
 #'
 #' @return data frame with a column with average methylation for each track, and another column with ".cov" suffix with every track's coverage. If \code{extract_meth_calls} is TRUE - additional columns with ".meth" suffix with number of methylation calls per track.
 #'
 #' @seealso \link[misha]{gextract}
 #' @export
-gextract_meth <- function(tracks, names = NULL, intervals = gintervals.all(), iterator = "intervs.global.seq_CG", d_expand = NULL, join_intervals = FALSE, extract_meth_calls = FALSE, ...) {
+gextract_meth <- function(tracks, names = NULL, intervals = gintervals.all(), iterator = "intervs.global.seq_CG", d_expand = NULL, join_intervals = FALSE, extract_meth_calls = FALSE, avg_tracks=NULL, avg_tracks_names=NULL, min_cov=NULL, ...) {
     opt <- options(gmax.data.size = 1e9)
     on.exit(options(opt))
 
@@ -36,15 +39,31 @@ gextract_meth <- function(tracks, names = NULL, intervals = gintervals.all(), it
     }
 
     if (extract_meth_calls) {
+        if (!is.null(avg_tracks)){
+            stop("cannot use 'extract_meth_calls' option with avg_tracks")
+        }
         data <- func(c(glue("{meth_vtracks} / {cov_vtracks}"), cov_vtracks, meth_vtracks), intervals = intervals, iterator = iterator, colnames = c(names, glue("{names}.cov"), glue("{names}.meth")), ...)
     } else {
-        data <- func(c(glue("{meth_vtracks} / {cov_vtracks}"), cov_vtracks), intervals = intervals, iterator = iterator, colnames = c(names, glue("{names}.cov")), ...)
+        if (!is.null(avg_tracks)){
+            avg_names <- avg_tracks_names %||% avg_tracks            
+            data <- func(c(glue("{meth_vtracks} / {cov_vtracks}"), cov_vtracks, glue("{avg_tracks}.avg")), intervals = intervals, iterator = iterator, colnames = c(names, glue("{names}.cov"), avg_names), ...)
+        } else {
+            data <- func(c(glue("{meth_vtracks} / {cov_vtracks}"), cov_vtracks), intervals = intervals, iterator = iterator, colnames = c(names, glue("{names}.cov")), ...)
+        }
+        
     }
 
     replace_list <- map(glue("{names}.cov"), ~0) %>% set_names(glue("{names}.cov"))
     data <- data %>% tidyr::replace_na(replace = replace_list)
 
     data <- as_tibble(data)
+
+    if (!is.null(min_cov)){
+        for (n in names){
+            f <- data[[glue("{n}.cov")]] < min_cov
+            data[[n]][f] <- NA
+        }
+    }    
 
     return(data)
 }
