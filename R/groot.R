1065dfd99fd47231e7b9851609e483cbc4974979
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-03-08 15:58:05 +0200

    read params_yaml from an env variable + added genome_exists function

--- R/groot.R
+++ R/groot.R
@@ -1,3 +1,5 @@
+#'
+#' @rdname  gset_genome
 find_params_yaml <- function() {
     if (file.exists(".misha.yaml")) {
         return(".misha.yaml")
@@ -6,6 +8,10 @@ find_params_yaml <- function() {
     home_dir_yaml <- file.path(Sys.getenv("HOME"), ".misha.yaml")
     if (file.exists(home_dir_yaml)) {
         return(home_dir_yaml)
+    }
+
+    if (!is.null(Sys.getenv("MISHA_GENOMES"))) {
+        return(Sys.getenv("MISHA_GENOMES"))
     }
 
     file.copy(system.file("config", "misha_params.yaml", package = "misha.ext"), to = home_dir_yaml)
@@ -20,7 +26,10 @@ init_config <- function(params_yaml) {
 #' Set misha root based on genome name
 #'
 #' @param genome name of the genome (e.g. hg19)
-#' @param params_yaml path to .misha.yaml parameters file
+#' @param params_yaml path to .misha.yaml parameters file. By default, \code{misha.ext} would look for such file
+#' first at the current directory, then at the user's home directory, then at an environment variable called "MISHA_GENOMES"
+#' and if none of the above exist - a new file would be created at the user's home directory. You can run \code{find_params_yaml}
+#' to see the current location of your configuration file.
 #'
 #' @examples
 #' \dontrun{
@@ -29,11 +38,25 @@ init_config <- function(params_yaml) {
 #'
 #' @export
 gset_genome <- function(genome, params_yaml = find_params_yaml()) {
-    init_config(params_yaml)
-    genomes <- tgconfig::get_param("genome", package = "misha.ext")
-    groot <- genomes[[genome]]
+    groot <- get_genome(genome, params_yaml)
     if (is.null(groot)) {
         stop("no genome named ", genome, " in params yaml")
     }
     gsetroot(groot)
+}
+
+get_genome <- function(genome, params_yaml) {
+    init_config(params_yaml)
+    genomes <- tgconfig::get_param("genome", package = "misha.ext")
+    groot <- genomes[[genome]]    
+    return(groot)
+}
+
+
+#' Does a genome db exist at the config file
+#'
+#' @inheritParams gset_genome
+#' @export
+genome_exists <- function(genome, params_yaml = find_params_yaml()) {
+    return(!is.null(get_genome(genome, params_yaml)))
 }
