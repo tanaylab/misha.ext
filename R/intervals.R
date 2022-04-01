b91adb8e288b86dd49d83e91af3d7482c584c48d
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-04-01 14:02:11 +0300

    added tss_intervals paramter to get_promoters

--- R/intervals.R
+++ R/intervals.R
@@ -78,12 +78,13 @@ gextract.left_join <- function(expr, intervals = NULL, colnames = NULL, iterator
 
 
 #' Define promoter regions
+#' @param tss_intervals name of the intervals set with the TSS
 #' @param upstream bp upstream to TSS
 #' @param downstream bp downstread from tss
 #'
 #' @export
-get_promoters <- function(upstream = 500, downstream = 50) {
-    gintervals.load("intervs.global.tss") %>%
+get_promoters <- function(upstream = 500, downstream = 50, tss_intervals = "intervs.global.tss") {
+    gintervals.load(tss_intervals) %>%
         mutate(start = ifelse(strand == 1, start - upstream, start - downstream), end = ifelse(strand == 1, end + downstream, end + upstream)) %>%
         gintervals.force_range()
 }
